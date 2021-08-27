7f577e1fc8dfdad2c5874b7dcd770b3a9ef3a45b
TimProger <akylverentimofei@mail.ru>
2021-08-28 01:09:02 +0300

    Upgraded game with 'break' function and attempt counter

--- scenes/game.js
+++ scenes/game.js
@@ -17,6 +17,7 @@ gameFirstStep.on("text", async (ctx) => {
 });
 const getNum = (max, min) => Math.floor(Math.random() * (max - min + 1)) + min;
 let num = getNum(100, 1);
+let counter = 0;
 const gameSecondStep = new Composer();
 gameSecondStep.on("text", async (ctx) => {
   try {
@@ -28,12 +29,17 @@ gameSecondStep.on("text", async (ctx) => {
       num = getNum(100, 1);
       await ctx.replyWithHTML(
         "Отлично, в таком случае скажи мне, какое <b>число</b> я загадал?",
-        Markup.removeKeyboard(true)
+        Markup.keyboard([["Сдаюсь!"]])
+          .oneTime()
+          .resize()
       );
       await ctx.replyWithHTML("Введите число от 1 до 100.");
       return ctx.wizard.next();
     } else {
-      await ctx.replyWithHTML("Нет так нет, уговаривать не буду.");
+      await ctx.replyWithHTML(
+        "Нет так нет, уговаривать не буду.",
+        Markup.removeKeyboard(true)
+      );
       return ctx.scene.leave();
     }
   } catch (e) {
@@ -44,17 +50,28 @@ const gameThirdStep = new Composer();
 gameThirdStep.on("text", async (ctx) => {
   try {
     ctx.wizard.state.data.num = ctx.message.text;
-    if (ctx.wizard.state.data.num === num.toString()) {
-      await ctx.replyWithHTML("🔥 Это <b>правильный</b> ответ, поздравляю! 🔥");
+    if (+ctx.wizard.state.data.num === num) {
+      await ctx.replyWithHTML(
+        `🔥 Это <b>правильный</b> ответ, поздравляю! 🔥 \nВаших попыток угадать ровно ${counter}`,
+        Markup.removeKeyboard(true)
+      );
       return ctx.scene.leave();
     } else if (+ctx.wizard.state.data.num > num) {
       await ctx.replyWithHTML(
-        "Не верно, загаданное число <b>МЕНЬШЕ</b> написанного. Попробуйте ещё раз!"
+        "Не верно, загаданное число <b>МЕНЬШЕ</b> написанного. Попробуйте ещё раз!",
+        counter++
       );
     } else if (+ctx.wizard.state.data.num < num) {
       await ctx.replyWithHTML(
-        "Не верно, загаданное число <b>БОЛЬШЕ</b> написанного. Попробуйте ещё раз!"
+        "Не верно, загаданное число <b>БОЛЬШЕ</b> написанного. Попробуйте ещё раз!",
+        counter++
       );
+    } else if (ctx.wizard.state.data.num === "Сдаюсь!") {
+      await ctx.replyWithHTML(
+        `🔥 Правильный ответ был ${num}, но вы можете попробовать снова, написав цифру 3! 🔥`,
+        Markup.removeKeyboard(true)
+      );
+      return ctx.scene.leave();
     }
   } catch (e) {
     console.log(e);
