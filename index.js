ff18241c40875a6eb3e96621a698c320daff3042
TimProger <akylverentimofei@mail.ru>
2021-09-18 19:12:02 +0300

    message sending

--- index.js
+++ index.js
@@ -96,7 +96,8 @@ bot.hears("2", async (ctx) => {
     `У меня есть небольшой список, который будет пополняться с каждым новым проектом над которым я работал.`
   );
   await ctx.replyWithHTML(
-    `Мои проекты: \n\n ⭐️ <i><b>Comics.hub</b></i> ⭐️\n\n ⭐️ <i><b>Telegraf</b></i> ⭐️
+    `Мои проекты: \n\n ⭐️ <i><b>Comics.hub</b></i> ⭐️\n\n ⭐️ <i><b>NoteWrite</b></i> ⭐️\n\n ⭐️ <i><b>Telegraf</b></i> ⭐️
+
         `,
     Markup.inlineKeyboard([
       [Markup.button.callback("Comics.hub", "comicshub")],
